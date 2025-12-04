30aac42a7fba87743e0f7c952f6c739d76f2a62a
RRC Fall 2023 emarcoux <143529657+emarcoux2@users.noreply.github.com>
2025-12-04 13:25:18 -0600

    Deleted models as it was making things confusing when trying to call data - it wants to use the SDK models.

--- app/src/main/java/com/example/tcgtracker/api/models/PokemonCardSetSeriesPreview.kt
+++
@@ -1,9 +0,0 @@
-package com.example.tcgtracker.api.models
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class PokemonCardSetSeriesPreview(
-    val id: String,
-    val name: String
-)
--- app/src/main/java/com/example/tcgtracker/api/models/submodels/PokemonCardAbility.kt
+++
@@ -1,9 +0,0 @@
-package com.example.tcgtracker.api.models.submodels
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class PokemonCardAbility(
-    val name: String,
-    val effect: String
-)
