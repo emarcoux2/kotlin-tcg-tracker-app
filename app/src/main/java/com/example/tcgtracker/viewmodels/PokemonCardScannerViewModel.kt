c25ae963dbe1f99f0996866498013a337e3a45ec
RRC Fall 2023 emarcoux <143529657+emarcoux2@users.noreply.github.com>
2025-12-10 21:14:50 -0600

    Commented out Scanner files as I don't have time to implement it right now.

--- app/src/main/java/com/example/tcgtracker/viewmodels/PokemonCardScannerViewModel.kt
+++ app/src/main/java/com/example/tcgtracker/viewmodels/PokemonCardScannerViewModel.kt
@@ -1,25 +1,25 @@
 package com.example.tcgtracker.viewmodels
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import androidx.lifecycle.ViewModel
-import com.example.tcgtracker.model.ScanType
-import com.example.tcgtracker.model.ScannedItem
-
-class PokemonCardScannerViewModel : ViewModel() {
-    // holds the list of scanned results
-    var items by mutableStateOf<List<ScannedItem>>(emptyList())
-        private set
-
-    // creates a new ScannedItem object using the provided type and content
-    // and adds the new item to the front of the list
-    fun addItem(type: ScanType, content: String) {
-        items = listOf(ScannedItem(type = type, content = content)) + items
-    }
-
-    // removes items all scanned items from the list
-    fun clear() {
-        items = emptyList()
-    }
-}
+//import androidx.compose.runtime.getValue
+//import androidx.compose.runtime.mutableStateOf
+//import androidx.compose.runtime.setValue
+//import androidx.lifecycle.ViewModel
+//import com.example.tcgtracker.model.ScanType
+//import com.example.tcgtracker.model.ScannedItem
+//
+//class PokemonCardScannerViewModel : ViewModel() {
+//    // holds the list of scanned results
+//    var items by mutableStateOf<List<ScannedItem>>(emptyList())
+//        private set
+//
+//    // creates a new ScannedItem object using the provided type and content
+//    // and adds the new item to the front of the list
+//    fun addItem(type: ScanType, content: String) {
+//        items = listOf(ScannedItem(type = type, content = content)) + items
+//    }
+//
+//    // removes items all scanned items from the list
+//    fun clear() {
+//        items = emptyList()
+//    }
+//}
